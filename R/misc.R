ea08a7125933bfd8a23c25cd0ce633cbdca26676
MerlinHeidmanns <mnh2123@columbia.edu>
2019-10-19 15:55:38 -0400

    added check family

--- R/misc.R
+++ R/misc.R
@@ -8,3 +8,21 @@
 #
 # @param x A character vector (probably x = family(fit)$family)
 is.gaussian <- function(x) x == "gaussian"
+
+
+# Check family argument
+#
+# @param f The \code{family} argument specified by user (or the default).
+# @return If no error is thrown, then either \code{f} itself is returned (if
+#   already a family) or the family object created from \code{f} is returned (if
+#   \code{f} is a string or function).
+validate_family <- function(f) {
+  if (is.character(f))
+    f <- get(f, mode = "function", envir = parent.frame(2))
+  if (is.function(f))
+    f <- f()
+  if (!is(f, "family"))
+    stop("'family' must be a family.", call. = FALSE)
+
+  return(f)
+}
