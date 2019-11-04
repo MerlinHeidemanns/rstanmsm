0036149da69456d6dc588cb6704c86c054bdd4b9
MerlinHeidmanns <mnh2123@columbia.edu>
2019-11-04 09:37:06 -0500

    fixed the no parameter problem

--- R/stan_msm.fit.R
+++ R/stan_msm.fit.R
@@ -2,18 +2,29 @@
 
 
 
-stan_msm.fit <- function(x_e, x_d, y = y, n, t, K = 2, has_intercept = c(0, 0),
+stan_msm.fit <- function(x_e, x_d, y = y, n, t, K = 2, has_intercept = c(0, 0), shared_TP = TRUE,
                          formula = parsed_formula, family = gaussian(), init.prior = TRUE,
                          algorithm = c("optimizing", "sampling"), ... = ...){
-
-    # Coerce to matrixes
-    x_e <- as.matrix(x_e)
-    x_d <- as.matrix(x_d)
 
     # NT
     N <- max(n)
     T <- max(t)
     NT <- N * T
+
+    # Coerce to matrixes
+    x_e <- as.matrix(x_e)
+    x_d <- as.matrix(x_d)
+
+    # dimensions
+    Mx_d <- ncol(x_d)
+    Mx_e <- ncol(x_e)
+
+    if (Mx_e == 0){
+      x_e <- matrix(0, ncol = 0, nrow = NT)
+    }
+    if (Mx_d == 0){
+      x_d <- matrix(0, ncol = 0, nrow = NT)
+    }
 
     # family
     family <- validate_family(family)
@@ -35,9 +46,6 @@ stan_msm.fit <- function(x_e, x_d, y = y, n, t, K = 2, has_intercept = c(0, 0),
       beta = formula$e
     )
 
-    # dimensions
-    Mx_d <- ncol(x_d)
-    Mx_e <- ncol(x_e)
 
     # standata
     standata <- list(
@@ -51,21 +59,29 @@ stan_msm.fit <- function(x_e, x_d, y = y, n, t, K = 2, has_intercept = c(0, 0),
        x_d = x_d,
        x_e = x_e,
        y = y,
-       has_intercept = has_intercept
+       has_intercept = has_intercept,
+       shared_TP = shared_TP
      )
 
     # stanfit
-    stanfit <- stanmodels$msm_constant_continuous # EXCHANGE
+    stanfit <- stanmodels$msm_constant_continuous_v2 # EXCHANGE
+
+    # parameters to exclude
+    pars <- pars_include(Mx_d = Mx_d, Mx_e = Mx_e)
 
     # initialization
+    N_ <- if (shared_TP) 1 else N
+    Mx_d_ <- if (has_intercept[1] == 1) Mx_d + 1 else if (Mx_d == 0) 1 else Mx_d
+    Mx_e_ <- if (has_intercept[2] == 1) Mx_e + 1 else if (Mx_e == 0) 1 else Mx_e
+
     if (init.prior) {
       initf2 <- function(chain_id = 1) {
         list(
           pi1 = matrix(rep(1/K, N * K), ncol = K, nrow = N),
-          lambda = rdirichlet(K, rep(1, K)),
+          A = array(rdirichlet(K * N_, rep(1, K)), dim = c(N_, K, K)),
           phi = matrix(sort(rnorm(K, 0.5, 0.25), decreasing = FALSE), ncol = K, nrow = 1),
           mu = matrix(sort(rnorm(K, 0, 5)), ncol = K, nrow = 1),
-          alpha = rnorm(Mx_d_, 0, 1),
+          alpha = array(rnorm(Mx_d_, 0, 1)),
           beta = matrix(rnorm(K * Mx_e_, 0, 1), ncol = K, nrow = Mx_e_),
           sigma = 1)
         }
@@ -74,6 +90,8 @@ stan_msm.fit <- function(x_e, x_d, y = y, n, t, K = 2, has_intercept = c(0, 0),
     } else {
       init = "random"
     }
+
+
 
     if (algorithm == "optimizing") {
       optimizing_args <- list(...)
@@ -88,7 +106,8 @@ stan_msm.fit <- function(x_e, x_d, y = y, n, t, K = 2, has_intercept = c(0, 0),
       if (algorithm == "sampling") {
           sampling_args <- set_sampling_args(
             object = stanfit,
-            #prior = prior,
+            pars = pars,
+            include = FALSE,
             user_dots = list(...),
             user_adapt_delta = adapt_delta,
             data = standata,
@@ -97,6 +116,7 @@ stan_msm.fit <- function(x_e, x_d, y = y, n, t, K = 2, has_intercept = c(0, 0),
       }
       check <- try(check_stanfit(stanfit))
       if (!isTRUE(check)) return(standata)
+
 
       return(structure(stanfit))
     }
