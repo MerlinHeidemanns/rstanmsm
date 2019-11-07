fd07728aa2bff59445dfcb4ed4b19f87f0cc23e7
MerlinHeidmanns <mnh2123@columbia.edu>
2019-11-07 11:25:49 -0500

    S3 print for stanmsm

--- R/print-and-summary.R
+++ R/print-and-summary.R
@@ -1,37 +1,52 @@
 
-
-
-
-coef.stan_msm <- function(object){
-  coef_discrete <- c()
-  coef_continuous <- c()
-  coef_continuous <- rbind(fit$coefs_median$alpha, fit$coefs_median$beta)
-  return(list(coef_discrete = coef_discrete, coef_continuous = coef_continuous))
+coef.stan_msm <- function(object, digits = 1, kind = NULL){
+  if (is.null(kind)){
+     x <- cbind(object$coefs_median$alpha, object$ses$alpha)
+     x <- rbind(x, cbind(object$coefs_median$beta, object$ses$beta))
+     x <- rbind(x, cbind(object$coefs_median$gamma, object$ses$gamma))
+     x <- rbind(x, cbind(object$coefs_median$delta, object$ses$delta))
+     x <- rbind(x, cbind(object$coefs_median$lambda, object$ses$lambda))
+  }
+  if (!is.null(kind)){
+    options <- c("alpha", "beta", "gamma", "delta", "lambda")
+    if (is.element(kind, options)){
+      x <- cbind(object$coefs_median[[kind]], object$ses[[kind]])
+    } else {
+      stop(cat("Please choose one of", paste0(options, sep = ","), "."))
+    }
+  }
+  colnames(x) <- c("Median", "MAD_SD")
+  return(round(x, digits = digits))
 }
 
+#' print.stan_msm
+#'
+#' @param object A stan_msm fit object.
+#' @param digits The number of digits to round to.
+#' @param detail Print output.
+#'
+#' A S3 function to print a summary output of a stan_msm object.
 
-
-#
-# print.stanreg_msm <- function(x, digits = 1, detail = TRUE, ...) {
-#   if (detail) {
-#     cat(x$stan_function)
-#     cat("\n Family:      ", family_plus_link(x))
-#     cat("\n Formula:     ", formula_string(formula(x)))
-#     cat("\n Observations:", nobs(x))
-#     if (isTRUE(x$stan_function %in%
-#                c("stan_msm"))) {
-#       cat("\n Predictors:  ", length(coef(x)$))
-#       cat("\n Time-varying Predictors:   ", )
-#     } else if (isTRUE(x$stan_function %in%
-#                c("stanmsm_tvtp")))
-#
-#     cat("\n------\n")
-#   }
-#
-#   if (detail) {
-#     cat("\n------\n")
-#     cat("* For help interpreting the printed output see ?print.stanreg\n")
-#     cat("* For info on the priors used see ?prior_summary.stanreg\n")
-#   }
-#   invisible(x)
-# }
+print.stan_msm <- function(object, digits = 1, detail = TRUE, ...){
+  if (detail){
+      cat(object$stan_function)
+      cat("\n Continuous formula:      ", object$formula_continuous)
+      cat("\n Observations:            ", length(object$data$n) )
+    if (length(unique(object$data$n)) > 1){
+      cat("\n Units:                   ",  length(unique(object$data$n)) )
+      cat("\n min(T):                  ",  length(unique(object$data$n)) )
+      cat("\n max(T):                  ",  length(unique(object$data$n)) )
+      cat("\n N(states)                ",  object$data$K)
+    }
+    cat("\n ----------------------------\n")
+    cat(" State-constant predictors\n ")
+    print(coef(object, digits = digits, kind = "alpha"))
+    cat("\n State-varying predictors\n ")
+    print(coef(object, digits = digits, kind = "beta"))
+  }
+  if (detail) {
+    cat("\n------\n")
+    cat("* For help interpreting the printed output see ?print.stan_msm\n")
+  }
+  invisible(x)
+}
